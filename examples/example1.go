972d4fbe38bfe5fc01a4cb92f9dcaf92b14bdd86
Steve Conklin <steve@conklinhouse.com>
2019-07-23 14:46:20 -0500

    change i2c bus

--- examples/example1.go
+++ examples/example1.go
@@ -16,7 +16,7 @@ func main() {
 	defer logger.FinalizeLogger()
 	// Create new connection to i2c-bus on 1 line with address 0x76.
 	// Use i2cdetect utility to find device address over the i2c-bus
-	i2c, err := i2c.NewI2C(0x48, 0)
+	i2c, err := i2c.NewI2C(0x48, 1)
 	if err != nil {
 		lg.Fatal(err)
 	}
