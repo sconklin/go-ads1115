7aded318a2a36688e0cc22b8626bb07caec030d4
Steve Conklin <steve@conklinhouse.com>
2019-08-05 17:28:15 -0500

    change to sconklin fork of logger

--- logger.go
+++ logger.go
@@ -1,10 +1,10 @@
 package ads
 
-import logger "github.com/d2r2/go-logger"
+import logger "github.com/sconklin/go-logger"
 
 // You can manage verbosity of log output
 // in the package by changing last parameter value.
 var logads = logger.NewPackageLogger("ads1115",
-	logger.DebugLevel,
-	// logger.InfoLevel,
+	// logger.DebugLevel,
+	logger.InfoLevel,
 )
