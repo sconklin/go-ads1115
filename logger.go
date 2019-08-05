e1f2dcd20478c6084080f5a4d86a270a7f34166b
Steve Conklin <steve@conklinhouse.com>
2019-08-05 16:26:31 -0500

    change name of logging variable

--- logger.go
+++ logger.go
@@ -4,7 +4,7 @@ import logger "github.com/d2r2/go-logger"
 
 // You can manage verbosity of log output
 // in the package by changing last parameter value.
-var lg = logger.NewPackageLogger("ads1115",
+var logads = logger.NewPackageLogger("ads1115",
 	logger.DebugLevel,
 	// logger.InfoLevel,
 )
